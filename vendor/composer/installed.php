8c211b08821439d9f9f6c5f54c98284b2abeac27
Blaaszkovsky <58946021+Blaaszkovsky@users.noreply.github.com>
2022-11-09 18:06:57 +0100

    First version module

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'prestashop-module',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ffc08180dc69d114cbc4b6b66af80796c664937c',
+        'name' => 'prestashop/ps_imageslider',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'prestashop/ps_imageslider' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'prestashop-module',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ffc08180dc69d114cbc4b6b66af80796c664937c',
+            'dev_requirement' => false,
+        ),
+    ),
+);
